fc20eed98565c3f25ea2112e5f2658981cc26c17
Kageyori <erhardtsimon21@gmail.com>
2025-04-01 17:32:58 +0200

    Initial commit

---
+++ src/main/java/net/kageyori/swordmacemod/SwordMaceModClient.java
@@ -0,0 +1,10 @@
+package net.kageyori.swordmacemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SwordMaceModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
